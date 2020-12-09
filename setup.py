6b1397de817589a96849c15dfe4f26acad012554
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2020-12-09 14:11:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="dash-keycloak",
-    version="0.0.18rc2",
+    version="0.0.18rc3",
     description="Flask extension providing Keycloak integration via the python-keycloak package",
     long_description=README,
     long_description_content_type="text/markdown",
